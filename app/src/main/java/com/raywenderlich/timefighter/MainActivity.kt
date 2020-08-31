e4296b90a9a7084d3ce164c4c2e8e07526d9c774
PunisherLex <alex.friedlander@gmail.com>
2020-08-30 20:41:30 -0400

    Chapter 4 Assignment 2 commit.
    Pt 3 of 4
    
    MainActivity.kt
    
        -Screen rotation score/time reset bug resolved.
    
        -basic logging assembled.
    
        -Retroactively implemented some functions I forgot in Chapter 3.

--- app/src/main/java/com/raywenderlich/timefighter/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/timefighter/MainActivity.kt
@@ -2,6 +2,7 @@ package com.raywenderlich.timefighter
 
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity //According to developer.android.com, android.support packaged libraries are deprecated and succeeded by androidx
 import android.widget.Button
 import android.widget.TextView
@@ -9,6 +10,9 @@ import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = MainActivity::class.java.simpleName
+
     private lateinit var gameScoreTextView: TextView
     private lateinit var timeLeftTextView: TextView
     private lateinit var tapMeButton: Button
@@ -21,9 +25,15 @@ class MainActivity : AppCompatActivity() {
     private var countDownInterval: Long = 1000
     private var timeLeft = 60
 
+    companion object {
+        private const val SCORE_KEY = "SCORE_KEY"
+        private const val TIME_LEFT_KEY = "TIME_LEFT_KEY"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate called. Score is: $score")
 
         gameScoreTextView = findViewById(R.id.game_score_text_view) //Note to self: figure out what R stands for in the name of mnemonics, if this can't be done, just read it as [R]ead, or maybe [R]untime, since it's created on run...
         timeLeftTextView = findViewById(R.id.time_left_text_view)
@@ -32,7 +42,50 @@ class MainActivity : AppCompatActivity() {
         tapMeButton.setOnClickListener{incrementScore()}
         // connect views to variables
 
-        resetGame()
+        if (savedInstanceState !=null){
+            score = savedInstanceState.getInt(SCORE_KEY)
+            timeLeft = savedInstanceState.getInt(TIME_LEFT_KEY)
+            restoreGame()
+        } else{
+            resetGame()
+        }
+    }
+    private fun restoreGame(){
+        val restoredScore = getString(R.string.your_score, score) //Integer.toString removed, doesn't work when following the Chapter instructions and is unnecessary bulk to the code
+        gameScoreTextView.text = restoredScore
+        val restoredTime = getString(R.string.time_left, timeLeft) //Integer.toString removed ^
+        timeLeftTextView.text = restoredTime
+
+        countDownTimer = object : CountDownTimer((timeLeft * 1000).toLong(),countDownInterval){
+            override fun onTick(millisUntilFinished: Long){
+                timeLeft = millisUntilFinished.toInt() / 1000
+
+                val timeLeftString = getString(R.string.time_left, timeLeft) //Integer.toString removed ^
+                timeLeftTextView.text = timeLeftString
+            }
+            override fun onFinish(){
+                endGame()
+            }
+        }
+        countDownTimer.start()
+        gameStarted = true
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle){
+        super.onSaveInstanceState(outState)
+
+        outState.putInt(SCORE_KEY, score)
+        outState.putInt(TIME_LEFT_KEY, timeLeft)
+        countDownTimer.cancel()
+
+        Log.d(TAG, "onSaveInstanceState: Saving Score: $score & Time Left: $timeLeft")
+    }
+
+    override fun onDestroy(){
+        super.onDestroy()
+
+        Log.d(TAG, "onDestroy called.")
     }
 
     private fun incrementScore(){
@@ -64,7 +117,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onFinish(){
-                // To be Implemented Later
+                endGame()
             }
         }
         gameStarted = false
@@ -75,7 +128,8 @@ class MainActivity : AppCompatActivity() {
         gameStarted = true
     }
 
-    private fun endgame(){
-        // end game logic
+    private fun endGame(){
+        Toast.makeText(this,getString(R.string.game_over_message,score), Toast.LENGTH_LONG).show()
+        resetGame()
     }
 }
